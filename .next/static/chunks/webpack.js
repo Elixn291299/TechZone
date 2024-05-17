6f7a2fe3691dbca32995b0371766b45c019dfc82
Elixn291299 <138745221+Elixn291299@users.noreply.github.com>
2024-05-16 22:24:29 -0600

    Conexion

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3cbecc1339e15f8"; }
+/******/ 		__webpack_require__.h = function() { return "b4b0778aecae0778"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/models/database.ts
+++ src/models/database.ts
@@ -3,8 +3,9 @@ import { createConnection, Connection } from 'mysql2/promise';
 
 export async function connect(): Promise<Connection> {
   return createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'ginga',
+    host: 'http://nasabytechzone.com',
+    user: 'techzone_users_admin',
+    password: 'LD9bdL^fyWi&',
+    database: 'techzone_user_files',
   });
 }
